1cb7db1134fd1c2ed7dde87451b1cafcc497021d
NeoDoggy <neodog0616@gmail.com>
2021-06-18 10:33:10 +0800

    210618upd

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ import pydot
 data = genfromtxt('qq.csv', delimiter=',')
 x=data[:,:-1]
 y=data[:,-1]
-xtr=x[:-200]
-ytr=y[:-200]
-xts=x[-200:]
-yts=y[-200:]
+xtr=x[:-100]
+ytr=y[:-100]
+xts=x[-100:]
+yts=y[-100:]
 ytr=np_utils.to_categorical(ytr,10)
 yts=np_utils.to_categorical(yts,10)
 model=Sequential()
-model.add(Dense(input_dim=8,units=128,activation='relu'))
-model.add(Dense(units=64,activation='relu'))
+model.add(Dense(input_dim=8,units=256,activation='relu'))
+model.add(Dense(units=128,activation='relu'))
 model.add(Dense(units=10,activation='softmax'))
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-train_history=model.fit(x=xtr,y=ytr,validation_split=0.2,epochs=1000,batch_size=600,verbose=2)
+train_history=model.fit(x=xtr,
+                        y=ytr,
+                        validation_split=0.2,
+                        epochs=1000,
+                        batch_size=600,
+                        verbose=2)
 #plt.plot(train_history.history['loss'])
 plt.plot(train_history.history['accuracy'])
 plt.show()
